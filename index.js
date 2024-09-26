359b3e8381151564bc61f4ead4f40a6a847c6d82
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-09-27 01:51:21 +0200

    added buttons and adjusted styling so menus overflows main container

--- index.js
+++ index.js
@@ -1,50 +1,20 @@
-const detailButtons = document.querySelectorAll(".details-button");
-const sunset = document.querySelector(".sunset-social-hour")
-const gourmet = document.querySelector(".gourmet-food")
-const violetVibes = document.querySelector(".violet-vibes")
-const tropicalJazz = document.querySelector(".tropical-jazz")
+const gridOne = document.querySelector(".grid-one");
+const gridTwo = document.querySelector(".grid-two")
+const foodButton = document.querySelector(".menu-button-food");
+const cocktailButton = document.querySelector(".cocktail-menu-button");
 
-let activeEvent = sunset;
 
-detailButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        
-        let hasClass = (button.classList.contains("sunset") ? "sunset" :
-               (button.classList.contains("gourmet") ? "gourmet" :
-               (button.classList.contains("violet") ? "violet" :
-               (button.classList.contains("tropical") ? "tropical" : "none"))));
-
-               switch(hasClass){
-                
-                case "sunset":
-                    activeEvent.classList.remove("show")
-                    activeEvent.classList.add("hidden");
-                    sunset.classList.remove("hidden")
-                    sunset.classList.add("show");
-                    activeEvent = sunset;
-                    break;
-
-                case "gourmet":
-                    activeEvent.classList.remove("show")
-                    activeEvent.classList.add("hidden");
-                    gourmet.classList.remove("hidden")
-                    gourmet.classList.add("show");
-                    activeEvent = gourmet;
-                    break;
-                case "violet":
-                    activeEvent.classList.remove("show")
-                    activeEvent.classList.add("hidden");
-                    violetVibes.classList.remove("hidden")
-                    violetVibes.classList.add("show");
-                    activeEvent = violetVibes;
-                    break;
-                case "tropical":
-                    activeEvent.classList.remove("show")
-                    activeEvent.classList.add("hidden");
-                    tropicalJazz.classList.remove("hidden")
-                    tropicalJazz.classList.add("show");
-                    activeEvent = tropicalJazz;
-                    break;
-               }
-    })
+foodButton.addEventListener("click", () => {
+    gridOne.style.transform = "translateX(-1100px)"
+    gridTwo.style.transform = "translateX(-1100px)"
 })
+
+cocktailButton.addEventListener("click", () => {
+    gridOne.style.transform = "translateX(0)"
+    gridTwo.style.transform = "translateX(0)"
+})
+
+
+
+
+
